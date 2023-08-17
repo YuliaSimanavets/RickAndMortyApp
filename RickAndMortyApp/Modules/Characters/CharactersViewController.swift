003578a49d49936888626103f8401e7b732bda16
YuliaSimanavets <mrs.simonovets@gmail.com>
2023-08-17 14:09:38 +0200

    created LaunchScreen

---
+++ RickAndMortyApp/Modules/Characters/CharactersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CharactersViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Yuliya on 17/08/2023.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+}
+
